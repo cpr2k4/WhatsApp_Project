1ff2e228e7d20a7e959643655238620bafa8c60c
Chinmay <chinmayregade25@gmail.com>
2024-03-24 10:56:20 +0530

    Changed db from atlas to local mongodb

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const Connection = async()=>{
-    const DB_URL = process.env.DB_URL;
+    // const DB_URL = process.env.DB_URL;
+    const DB_URL = "mongodb://127.0.0.1:27017/whatsapp";
     try{
         await mongoose.connect(DB_URL);
         console.log("Connected to DB...");
@@ -13,5 +14,16 @@ const Connection = async()=>{
         console.log("Error while connecting to mongoDB",err.message);
     }
 }
+
+//main() for mongodb connection
+// main()
+// .then((res)=>{
+//     console.log("Connection established.....");
+// })
+// .catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+// }
 
 export default Connection;
